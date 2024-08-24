3f8288781564db9c585350c3798c68a2ca3c1c8c
imran1khan <107169527+imran1khan@users.noreply.github.com>
2024-08-24 20:29:08 +0530

    some new improve2

--- components/ImageFormatChange/ChangeFormet.tsx
+++ components/ImageFormatChange/ChangeFormet.tsx
@@ -1,4 +1,4 @@
-import { ImageFileAtom } from '@/packages/store/atoms/ImageFileAtom';
+// import { ImageFileAtom } from '@/packages/store/atoms/ImageFileAtom';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { Button } from '../ui/button';
@@ -10,15 +10,17 @@ interface Prop {
 }
 
 function ChangeFormet({ title, convertionType }: Prop) {
-  const imageFile = useRecoilValue(ImageFileAtom);
+  // const imageFile = useRecoilValue(ImageFileAtom);
 
   const fileDownload = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-    if (!imageFile) {
-      return;
-    }
-    const filename = imageFile.name.split('.')[0];
-    const url = await convertImageFormet(imageFile,convertionType);
+    // if (!imageFile) {
+    //   return;
+    // }
+    // const filename = imageFile.name.split('.')[0];
+    // const url = await convertImageFormet(imageFile,convertionType);
+    const filename='';
+    const url = ''
     downloadImage(url,filename+`.${convertionType==='jpeg'?'jpg':'png'}`)
   };
 
